0cfd1dfc321f5da8bc6b33aa2bf726ca603d19d3
Roman <romtega88@gmail.com>
2015-10-20 23:16:15 -0500

    Added a user id to tasks, configured user task associations

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,73 +1,47 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
+respond_to :html
+  
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
+    respond_with(@tasks)
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
+ def show
+    respond_with(@tasks)
   end
 
-  # GET /tasks/new
   def new
     @task = Task.new
+    respond_with(@tasks)
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
-    @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    @task = current_user.tasks.new(task_params)
+    @task.save
+    respond_with(@tasks) 
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    @task.update(task_params)
+    respond_with(@task)
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@task)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:content)
     end
